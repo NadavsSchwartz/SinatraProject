9dc2459de3173c86c3573682e5732665ee0c7a09
nadavschwartz <nadavschwartz58@gmail.com>
2020-08-08 11:59:38 -0700

    CRUD for flights added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,14 +20,13 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  
   helpers do
     def logged_in?
       !!session[:user_id]
     end
 
     def current_user
-      User.find(session[:user_id])
+      User.find_by(id: session[:user_id])
     end
 
     def find_user_by_id
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -10,8 +10,10 @@ class FlightController < ApplicationController
     origin = params[:origin].split(' ,')[1]
     destination = params[:destination].split(' ,')[1]
     @flights = Flight.get_flight(origin, destination, params[:depart_date])
-    if @flights['Quotes'].empty? || !!@flights['message']
+    if @flights['ValidationErrors'] || !!@flights['message']
       flash[:error] = "We Couldn't find any flights. Please go back and try again"
+    elsif @flights['Quotes'].empty?
+      flash[:error] = 'The search came empty. Please go back and try again'
     else
       flights = Flight.set_flight
       @user = current_user
@@ -20,29 +22,28 @@ class FlightController < ApplicationController
     end
   end
 
-  get '/flights/:id' do
-    @flight = Flight.find_by(id: params[:id])
-    @user = current_user
-    @cart = Cart.new(user_id: @user.id, flight_id: @flight.id)
-    if @cart.save
-      erb :'flights/show'
+  post '/flights/:id' do
+    if current_user.nil? || @flight = Flight.find_by(id: params[:id]).nil?
+      flash[:error] = 'You need to be logged in and have the flight added to your account, in order to see/edit/delete flights'
     else
-      flash[:error] = 'We encountered a problem.'
+      @user = current_user
+      @flight = Flight.find_by(id: params[:id])
+      @cart = Cart.find_or_create_by(user_id: @user.id, flight_id: @flight.id)
+      erb :'flights/show' if @cart.save
     end
   end
 
   get '/flights/edit/:id' do
-    if current_user.carts.find_by(user_id: current_user.id, flight_id: params[:id])
-      @airport = Airport.all
-      @user = current_user
-      @current_flight = @user.flights.find(params[:id])
-      erb :'flights/edit_flight'
+    if current_user.nil? || !current_user.carts.find_by(user_id: current_user.id, flight_id: params[:id])
+      flash[:error] = 'You need to be logged in and have the flight added to your account, in order to see/edit/delete flights'
     else
-      flash.now[:error] = 'Error occured. please refresh the page and try again.'
+      @airport = Airport.all
+      @current_flight = current_user.flights.find(params[:id])
+      erb :'flights/edit_flight'
     end
   end
 
-  post '/flights/:id' do
+  post '/flights/update/:id' do
     origin = params[:origin].split(' ,')[1]
     destination = params[:destination].split(' ,')[1]
     @flights = Flight.get_flight(origin, destination, params[:depart_date])
@@ -63,14 +64,21 @@ class FlightController < ApplicationController
   end
 
   get '/flights/delete/:id' do
-    binding.pry
-    if current_user.carts.find_by(user_id: current_user.id, flight_id: params[:id])
-      @flight = Flight.find(params[:id])
-      @flight.destroy
-      flash.now[:error] = 'Flight deleted.'
-      redirect to '/users/account'
+    if !logged_in?
+      flash[:error] = 'You need to be logged in, in order to edit/delete flights'
     else
-      flash.now[:error] = "Error occured. You can not delete a flight that wasn't added to your account."
+      if current_user.carts.find_by(user_id: current_user.id, flight_id: params[:id])
+        if current_user.flights.ids.include? params[:id].to_i
+          flight = Flight.find_by(id: params[:id])
+          flight.destroy
+          flash.now[:error] = 'Flight deleted.'
+          redirect to '/users/account'
+        else
+          flash.now[:error] = 'You already deleted that flight from your account.'
+        end
+      else
+        flash.now[:error] = "Error occured. You can not delete a flight that doesn't exist in your profile account."
+        end
     end
   end
 end
