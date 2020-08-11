28e2f9ad96db22ed050e3a36ed20f9584b0c6a4c
nadavschwartz <nadavschwartz58@gmail.com>
2020-08-10 17:12:45 -0700

    trial

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,8 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || {
                                           adapter: 'postgresql',
                                           database: "db/#{ENV['SINATRA_ENV']}.sqlite",
-                                          pool: 10
+                                          pool: 10,
+                                          timeout: 10_000
                                         })
 
 require './app/controllers/application_controller'
